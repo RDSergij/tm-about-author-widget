20d7bcf5bce0addd37271797fdbfa6d0f840c53f
RDSergij <sergijlys@gmail.com>
2016-01-15 10:49:07 +0200

    Style guide

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,7 +1,6 @@
 /**
  * Events list
  */
-//jQuery( document ).ready( initWidget );
 jQuery( document ).on( 'widget-updated widget-added ready', initWidget );
 
 /**
@@ -13,23 +12,25 @@ function initWidget() {
 
 	// Upload image
 	jQuery( '.tm-about-author-form-widget input[type=button].upload_image_button' ).on( 'click', function( e ) {
-		e.preventDefault();
 		var _this = jQuery( this );
 		var inputImage = _this.parents( '.tm-about-author-form-widget' ).find( '.custom-image-url' );
 		var inputAvatar = _this.parents( '.tm-about-author-form-widget' ).find( '.avatar img' );
-		var custom_uploader = wp.media( {
+		var customUploader = wp.media( {
 			title: 'Upload a Image',
 			button: {
 				text: 'Select',
 			},
 			multiple: false
 		} );
-		custom_uploader.on('select', function() {
-			var imgurl = custom_uploader.state().get( 'selection' ).first().attributes.url;
+
+		e.preventDefault();
+
+		customUploader.on( 'select', function() {
+			var imgurl = customUploader.state().get( 'selection' ).first().attributes.url;
 			inputImage.val( imgurl ).trigger( 'change' );
 			inputAvatar.attr( 'src', imgurl );
 		});
-		custom_uploader.open();
+		customUploader.open();
 	});
 
 	// Delete image
